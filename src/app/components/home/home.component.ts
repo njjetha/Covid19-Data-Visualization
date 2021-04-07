95b546454035b2df93dcc3aa75bffc70b38a6b8c
njjetha <neeraj.jetha@iiitb.org>
2021-04-07 21:32:14 +0530

    Charts modified and various cases data can be seen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { GoogleChartInterface } from 'ng2-google-charts';
 import { DataServiceService } from 'src/app/services/data-service.service';
 
@@ -8,8 +8,13 @@ import { DataServiceService } from 'src/app/services/data-service.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('mychart1 ', {static: false}) mychart1:any;
+  @ViewChild('mychart2 ', {static: false}) mychart2:any;
 
   tempConfirmed:number=0;
+  tempActive:number=0;
+  tempRecovered:number=0;
+  tempDeath:number=0;
   totalConfirmed=0;
   totalActive=0;
   totalDeath=0;
@@ -19,6 +24,10 @@ export class HomeComponent implements OnInit {
   Death:any;
   Recovered:any;
   database:any[]=[];
+  database_confirmed:any[]=[];
+  database_active:any[]=[];
+  database_recovered:any[]=[];
+  database_deaths:any[]=[];
   public pieChart: GoogleChartInterface = {
     chartType: 'PieChart',
   };
@@ -29,22 +38,30 @@ export class HomeComponent implements OnInit {
   constructor(private dataservice:DataServiceService) { }
 
 
-  initChart(){
+  initChart(caseType:string){
+    // console.log(caseType);
+    
+    if(caseType=='c')
+      this.database=this.database_confirmed;
+    if(caseType=='a')
+      this.database=this.database_active;
+    if(caseType=='r')
+      this.database=this.database_recovered;
+    if(caseType=='d')
+      this.database=this.database_deaths;
+  
+
+  //  console.log(this.database);
+
     this.pieChart={
       chartType: 'PieChart',
       // dataTable: this.database,
       dataTable:this.database,
       //firstRowIsData: true,
        options: {
-        "backgroundColor": '#f1f8e9',
-        width: '100px',
-        chartArea: {
-            left: "5%",
-            top: "20%",
-            bottom:"10%",
-            height: "1000",
-            width: "1000"
-        }            
+        backgroundColor: '#f1f8e9',
+        width: 1200,
+        height:500,           
         },
     };
   
@@ -57,16 +74,18 @@ export class HomeComponent implements OnInit {
         height:500,
         width:1200
       },
-  };
-
-   console.log(this.database);
+    };
+    // console.log(this.pieChart.dataTable);
   }
 
 
 
 
   ngOnInit(): void {
-    this.database.push(["State","Cases"]);
+    this.database_confirmed.push(["State","Confirmed Cases"]);
+    this.database_active.push(["State","Active Cases"]);
+    this.database_recovered.push(["State","Recovered Cases"]);
+    this.database_deaths.push(["State","Death Cases"]);
     this.dataservice.getCovidData().
       subscribe({
         next:(result)=>{
@@ -75,13 +94,19 @@ export class HomeComponent implements OnInit {
             let y=result[i].districtData;
             //  console.log(y);
             this.tempConfirmed=0;
+            this.tempActive=0;
+            this.tempRecovered=0;
+            this.tempDeath=0;
             for (var key in y) {
               if (y.hasOwnProperty(key)) {
                 // console.log("confirmed ->"+y[key].confirmed)
                 this.tempConfirmed+=y[key].confirmed;
                 this.totalConfirmed+=(y[key].confirmed);
+                this.tempActive+=y[key].active
                 this.totalActive+=y[key].active;
+                this.tempDeath+=y[key].deceased; 
                 this.totalDeath+=y[key].deceased;
+                this.tempRecovered+=y[key].recovered;
                 this.totalRecovered+=y[key].recovered;
               }
             }
@@ -93,15 +118,29 @@ export class HomeComponent implements OnInit {
             this.Death=this.totalDeath.toLocaleString('en-IN',{maximumFractionDigits:2});
             this.Recovered=this.totalRecovered.toLocaleString('en-IN',{maximumFractionDigits:2});
             // console.log(this.Confirmed);
-            this.database.push([this.dataservice.getStateName(i),this.tempConfirmed]);
+            this.database_confirmed.push([this.dataservice.getStateName(i),this.tempConfirmed]);
+            this.database_active.push([this.dataservice.getStateName(i),this.tempActive]);
+            this.database_recovered.push([this.dataservice.getStateName(i),this.tempRecovered]);
+            this.database_deaths.push([this.dataservice.getStateName(i),this.tempDeath]);
             
           }
           // console.log(this.database);
-          this.initChart();
+          this.initChart('c');
         }
       });
 
   }
+
+  updateChart(input:HTMLInputElement){
+    // console.log(input.value);
+    this.initChart(input.value);
+    this.mychart1.draw();
+    this.mychart2.draw();
+   
+  }
+
+
+
 
 
 }
